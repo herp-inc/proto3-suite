8db6cfd718688b0383fbca41ac7cfbe432763003
kgtkr <contact@kgtkr.net>
2022-02-08 19:25:41 +0900

    fixup! Add `pkgs` / `nixpkgs` options to `default.nix`

--- default.nix
+++ default.nix
@@ -7,13 +7,7 @@
     inherit nixpkgs compiler enableDhall enableSwagger swaggerWrapperFormat;
   }
 }:
-
-let
-  pkgs = import ./nix/pkgs.nix {
-    inherit compiler enableDhall enableSwagger swaggerWrapperFormat;
-  };
-
-in {
+{
   proto3-suite-boot = pkgs.haskellPackages.proto3-suite-boot;
 
   proto3-suite = pkgs.haskellPackages.proto3-suite;
